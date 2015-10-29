5a6a74c93a09de951b4daef7b413fecdd0c1bf51
Eduard Lorente <elorente@pisos.com>
2015-10-29 17:00:12 +0100

    LDR_Test (Falta comprobar amb circuit muntat)

---
+++ Arduino/examples/LDR_Test/LDR_Test.ino
@@ -0,0 +1,32 @@
+
+int analogPin = 0;
+int redLedPin = 13;
+int greenLedPin = 12;
+
+int light=150;//Intervalo de luz. Testear y cambiarlo por el de vuestro entorno
+
+void setup(){
+  //inputs
+  pinMode(analogPin,INPUT);
+  
+  //outputs
+  pinMode(redLedPin,OUTPUT);
+  pinMode(greenLedPin,OUTPUT);
+}
+  
+void loop(){
+  
+  //Valor del sensor de luz
+  int LDR_Value = analogRead(analogPin);
+  
+  if(LDR_Value>=45){
+	digitalWrite(greenLedPin, HIGH);
+  } else {
+	digitalWrite(redLedPin, HIGH);
+  }
+    
+  delay(700);
+  digitalWrite(redLedPin, LOW);
+  digitalWrite(greenLedPin, LOW);
+  delay(700);
+}
